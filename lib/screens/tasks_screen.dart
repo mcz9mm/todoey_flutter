789b63f67353953597b897a2acad2a0577312856
mcz9mm <k.m.swim411@gmail.com>
2020-01-14 08:02:13 +0900

    tasks_screen imprement header

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -8,9 +8,28 @@ class TasksScreen extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Icon(Icons.list),
-            Text('Todoey'),
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Todoey',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
           ],
         ),
       ),
