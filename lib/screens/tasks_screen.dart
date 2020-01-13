12d94ad9a7357441905716b8930311997c54a6d8
mcz9mm <k.m.swim411@gmail.com>
2020-01-14 07:54:22 +0900

    scaffold → container

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,7 +5,8 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: SafeArea(
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
         child: Column(
           children: <Widget>[
             Icon(Icons.list),
